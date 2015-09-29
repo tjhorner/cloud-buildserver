b429e1a5d55a0ff6d544a22991b44fb3284656a5
TJ Horner <me@tjhorner.com>
2015-09-28 17:55:46 -0700

    ayy

--- app.js
+++ app.js
@@ -13,10 +13,12 @@ app.get('/status', function(req, res){
 
 function logRemote(message, socket, tag){
   console.log(message.trim());
-  socket.emit("stdout", {stdout: message, tag: tag});
+  // TODO actually fix this bug
+  if(message.trim() !== ".") socket.emit("stdout", {stdout: message, tag: tag});
 }
 
-function build(scriptIndex, socket){
+function build(scriptIndex, socket, all){
+  if(!all) all = true;
   try{
     if(config.scripts[scriptIndex]){
       var scriptName = config.scripts[scriptIndex],
@@ -35,7 +37,13 @@ function build(scriptIndex, socket){
         if(code !== 0){
           logRemote("WARNING: Script exited with non-zero exit code. Please check.".red, socket);
         }
-        build(scriptIndex + 1, socket);
+        if(all){
+          build(scriptIndex + 1, socket);
+        }else{
+          logRemote("Script run complete, bye!", socket);
+          socket.emit("build:complete");
+          socket.disconnect();
+        }
       });
     }else{
       logRemote("Build complete, bye!", socket);
@@ -74,6 +82,17 @@ io.on('connection', function(socket){
     }
   });
 
+  socket.on("script:run", function(script){
+    if(socket.authenticated){
+      // run the script specified by
+      // the user
+      var script;
+      if(config.scripts.indexOf(script) !== -1) script = config.scripts.indexOf(script);
+      if(!script) socket.emit("script:noexist"); return;
+      build(script, socket, false);
+    }
+  });
+
   socket.emit("key:send");
 
   // authentication timeout
@@ -84,6 +103,10 @@ io.on('connection', function(socket){
       socket.disconnect();
     }
   }, 5000);
+});
+
+app.get("/", function(req, res){
+  res.redirect("https://github.com/tjhorner/cloud-buildserver");
 });
 
 http.listen(config.listen_port);
